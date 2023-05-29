6160c4f8fcf1a8644bf7fb3c8acca77feab79bb1
Joe <joesidious@gmail.com>
2023-05-29 19:22:32 +0100

    dayBetweenDates quiz completed

--- DaysBetweenDates_Quiz.java
+++ DaysBetweenDates_Quiz.java
@@ -39,4 +39,145 @@ int daysInMonth_ly(int year, int month)
         return daysInMonth(month);
     }
 }
+
+int[] nextDay(int year, int month, int day)
+//Function moves date to the next day
+{
+   
+    int day_c = daysInMonth_ly(year, month);
+
+    if (day < day_c)
+    {
+     day +=1;
+    }
+    else if ((day == day_c) && (month < 12 ))
+    {
+    day = 1;
+    month +=1;
+    }
+    else if ((day == 31) && (month == 12))
+    {
+    day = 1;
+    month =1;
+    year +=1;
+    }
+
+    int[] date = {year, month, day};
+
+    return date;
+}
+
+int daysleft(int year, int month,int day)
+{
+    int dayleft = daysInMonth_ly(year, month);
+     return dayleft;
+}
+
+int daysBtwnMonth(int year, int month1, int month2)
+{
+  int total_days = 0;
+  
+  for(int i = month1; i < month2; i++)
+  {
+    if ((isLeapYear(year)) && (i == 2))
+    {
+      total_days +=29;
+    }
+    else
+    {
+      total_days += daysInMonth(i);
+    }
+  }
+  return total_days;
+}
+
+int daysBtwnYear(int year1,int year2)
+//Calculate the number of days between two years
+{
+  int total_days = 0;
+  int days = 0;
+  year1 +=1;
+
+  for(int i = year1; i < year2; i++)
+  {
+    if (isLeapYear(i))
+    {
+      days +=366;
+    }
+    else{
+      days+=365;
+    }
+
+  }
+total_days +=days;
+return total_days;
+
+}
+
+int dayRemYear(int month,int year)
+{
+  int t_days =0;
+  int day = 0;
+  month+=1;
+  if (month <= 12)
+  {
+    for(int i=month; i<13; i++)
+    {
+      if (isLeapYear(year) && (i == 2))
+      {
+         day = 29;
+      }
+      else if (i == 2)
+      {
+        day = 28;
+      }
+      else if ((i == 4) || (i==6)||(i==9)||(i==11))
+      {
+        day = 30;
+      }
+      else 
+      {
+        day = 31;
+      }
+      t_days +=day;
+    }
+   
+  }
+
+  return t_days;
+}
+
+int sec_yr(int year, int month){
+  int day = 0;
+  int t_day = 0;
+  
+  for (int i = 1; i < month; i++){
+
+    day = daysInMonth_ly(year, i);
+    t_day +=day;
+  }
+
+  return t_day;
+}
+
+int dayBetweenDate(int year2, int month2, int day2, int year1, int month1, int day1){
+  int day_num = 0;
+  int day_month1 = daysInMonth_ly(year1, month1);
+  int day_month2 = daysInMonth_ly(year2, month2);
+  int day_years = daysBtwnYear(year1, year2);
+  
+  if ((year2 == year1) && (month2 == month1)){
+    day_num = day2 - day1;
+  }
+  else if ((year2 == year1) && (month2 > month1)){
+    day_num = daysBtwnMonth(year1,month2, month1) + (day_month2 - (day_month2 - day2));
+  }
+  else if ((year2 > year1)){
+    day_num = day_years + (day_month1 - day1) + dayRemYear(month1, year1) + sec_yr(year2, month2) + (day_month2 - (day_month2 - day2));
+  }
+
+
+  return day_num;
+}
+
 }
--- Main.java
+++ Main.java
@@ -9,8 +9,33 @@ public class Main {
 
         String day_val = Boolean.toString(lp);
         System.out.println(day_val);
-
+    
         int monthval = quiz1.daysInMonth(2);
+
+        int[] nextDay = quiz1.nextDay(2012, 5, 31);
+
+        int day_month = quiz1.daysBtwnMonth(2020,1,4);
+
+        int day_year = quiz1.daysBtwnYear(2012, 2017);
+
+        int month_year = quiz1.dayRemYear(1, 2020);
+
+        int sec = quiz1.sec_yr(2020, 3);
+
+        int nodays = quiz1.dayBetweenDate(2012, 6, 30, 2011, 6, 30);
+
+        System.out.println("the dayBetweenDate value is: " + nodays);
+
+        System.out.println("The value of the days in the second year before the month date is: " + sec);
+
+
+        System.out.println("The Day remainining in the year " + month_year);
+
+        System.out.println("The days between the year " + day_year);
+
+        System.out.println("The days between month is " + day_month);
+
+        System.out.println("print next day out " + nextDay[0] + " " + nextDay[1] + " " + nextDay[2]);
 
         System.out.println("The value of month val is: " + monthval);
 
